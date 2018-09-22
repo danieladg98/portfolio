e0c162c778741ecdde6401803d3aa2b3db3f0488
danieladg98 <33914495+danieladg98@users.noreply.github.com>
2018-09-22 20:37:27 +0100

    legendary

---
+++ legendary/index.js
@@ -0,0 +1,101 @@
+$(document).ready(function () {
+
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() < 200) {
+            $("body").css('background', '#3500D3');
+        }
+        if ($(this).scrollTop() > 200 && $(this).scrollTop() < 400) {
+            $("body").css('background', '#6600D3');
+        }
+        if ($(this).scrollTop() > 400 && $(this).scrollTop() < 600) {
+            $("body").css('background', '#7d04cb');
+        }
+        if ($(this).scrollTop() > 600 && $(this).scrollTop() < 800) {
+            $("body").css('background', '#FF180A');
+        }
+        if ($(this).scrollTop() > 800 && $(this).scrollTop() < 1000) {
+            $("body").css('background', '#FC452B');
+        }
+        if ($(this).scrollTop() > 1000 && $(this).scrollTop() < 1200) {
+            $("body").css('background', '#FF180A');
+        }
+        if ($(this).scrollTop() > 1200 && $(this).scrollTop() < 1400) {
+            $("body").css('background', '#7d04cb');
+        }
+        if ($(this).scrollTop() > 1400 && $(this).scrollTop() < 1600) {
+            $("body").css('background', '#6600D3');
+        }
+        if ($(this).scrollTop() > 1600 && $(this).scrollTop() < 1800) {
+            $("body").css('background', '#3500D3');
+        }
+        if ($(this).scrollTop() > 1800 && $(this).scrollTop() < 2000) {
+            $("body").css('background', '#6600D3');
+        }
+        if ($(this).scrollTop() > 2000 && $(this).scrollTop() < 2200) {
+            $("body").css('background', '#7d04cb');
+        }
+        if ($(this).scrollTop() > 2200 && $(this).scrollTop() < 2400) {
+            $("body").css('background', '#FF180A');
+        }
+        if ($(this).scrollTop() > 2400 && $(this).scrollTop() < 2600) {
+            $("body").css('background', '#FC452B');
+        }
+        if ($(this).scrollTop() > 2600 && $(this).scrollTop() < 2800) {
+            $("body").css('background', '#FF180A');
+        }
+        if ($(this).scrollTop() > 2800 && $(this).scrollTop() < 3000) {
+            $("body").css('background', '#7d04cb');
+        }
+
+        if ($(this).scrollTop() > 3000 && $(this).scrollTop() < 3200) {
+            $("body").css('background', '#6600D3');
+        }
+        if ($(this).scrollTop() > 3200 && $(this).scrollTop() < 3400) {
+            $("body").css('background', '#3500D3');
+        }
+        if ($(this).scrollTop() > 3400 && $(this).scrollTop() < 3600) {
+            $("body").css('background', '#6600D3');
+        }
+        if ($(this).scrollTop() > 3600 && $(this).scrollTop() < 3800) {
+            $("body").css('background', '#7d04cb');
+        }
+        if ($(this).scrollTop() > 3800 && $(this).scrollTop() < 4000) {
+            $("body").css('background', '#FF180A');
+        }
+        if ($(this).scrollTop() > 4000 && $(this).scrollTop() < 4200) {
+            $("body").css('background', '#FC452B');
+        }
+        if ($(this).scrollTop() > 4200 && $(this).scrollTop() < 4400) {
+            $("body").css('background', '#FF180A');
+        }
+        if ($(this).scrollTop() > 4400 && $(this).scrollTop() < 4600) {
+            $("body").css('background', '#7d04cb');
+        }
+
+        if ($(this).scrollTop() > 4600 && $(this).scrollTop() < 4800) {
+            $("body").css('background', '#6600D3');
+        }
+        if ($(this).scrollTop() > 4800 && $(this).scrollTop() < 5000) {
+            $("body").css('background', '#3500D3');
+        }
+        if ($(this).scrollTop() > 5000 && $(this).scrollTop() < 5200) {
+            $("body").css('background', '#6600D3');
+        }
+        if ($(this).scrollTop() > 5200 && $(this).scrollTop() < 5400) {
+            $("body").css('background', '#7d04cb');
+        }
+        if ($(this).scrollTop() > 5400 && $(this).scrollTop() < 5600) {
+            $("body").css('background', '#FF180A');
+        }
+        if ($(this).scrollTop() > 5600 && $(this).scrollTop() < 5800) {
+            $("body").css('background', '#FC452B');
+        }
+        if ($(this).scrollTop() > 5800 && $(this).scrollTop() < 6000) {
+            $("body").css('background', '#FF180A');
+        }
+        if ($(this).scrollTop() > 6000 && $(this).scrollTop() < 6200) {
+            $("body").css('background', '#7d04cb');
+        }
+    });
+});
